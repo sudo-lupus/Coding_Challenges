39ca478bd7bc59de95a9d02e15bb21dc29dea196
sudo-lupus <67869327+sudo-lupus@users.noreply.github.com>
2023-03-03 07:51:06 +0100

    Added 8th and 7th Kyu solutions

---
+++ codewars/8_kyu_2.py
@@ -0,0 +1,9 @@
+'''
+Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1]
+'''
+
+def reverse_seq(n):
+    return list(reversed([i for i in range(1,n+1)]))
+    
