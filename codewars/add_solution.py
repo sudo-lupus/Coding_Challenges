8eb2f9c9cd08c1c0c16a06dec3a950bfede3ef36
sudo-lupus <67869327+sudo-lupus@users.noreply.github.com>
2023-03-18 15:07:16 +0100

    Add File Ending Feature

--- codewars/add_solution.py
+++ codewars/add_solution.py
@@ -13,14 +13,25 @@ def kyu_dir(kyu):
         print("There is no folder: " + dir)
         return None
 
-def new_file(kyu_dir, ending = ".py"):
+def new_file(kyu_dir, file_ending):
     files = " ".join(os.listdir(kyu_dir))
     file_numeration = [int(num) for num in re.findall(r"_(\d+)\.*", files)]
     new_file_num = max(file_numeration) + 1
-    new_file_name = f"{kyu_dir}_{new_file_num}{ending}"
+    new_file_name = f"{kyu_dir}_{new_file_num}{file_ending}"
     new_file_loc = os.path.join(kyu_dir, new_file_name)
     with open(new_file_loc, "w+"):
         print("Successfully created {} in dir:\n{}".format(new_file_name, kyu_dir))
 
-dir = sys.argv[1]
-new_file(kyu_dir(dir))
+try:
+    dir = sys.argv[1]
+except IndexError:
+    print("You haven't supplied a kyu difficulty.")
+    sys.exit(1)
+
+try:
+    ending = sys.argv[2]
+except IndexError:
+    print("You haven't supplied an appropriate file ending.\nWorking with default .py file ending.")
+    ending = ".py"
+
+new_file(kyu_dir(dir), ending)
