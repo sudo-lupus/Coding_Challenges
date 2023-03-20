4295edc886a546921af815195a7858b9a09a3a48
sudo-lupus <67869327+sudo-lupus@users.noreply.github.com>
2023-03-20 17:45:23 +0100

    Changed Exit Codes 7_kyu_10.sh

--- codewars/7_kyu/7_kyu_10.sh
+++ codewars/7_kyu/7_kyu_10.sh
@@ -4,14 +4,14 @@
 if [ $# -eq 0 ]
 then
     echo "Nothing to find";
-    exit 0;
+    exit 1;
 fi
 
 if ls | grep -q $1
 then
     echo "Found $1";
-    exit 1;
+    exit 0;
 else
     echo "Can't find $1"
-    exit 0;
+    exit 1;
 fi
